2b2f3c2002c004f994ffe652c977b4b97239c116
Jay Do <jaydo1204@gmail.com>
2018-12-16 23:22:02 +0900

    SPA part2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({selectedDish}) {
   return (
@@ -36,14 +37,30 @@ function RenderComments({selectedComments}) {
 const DishDetail = (props) => {
   if (props.dish != null) {
     return (
+      <React.Fragment>
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to='/menu'>Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>
+            {props.dish.name}
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row">
         <div className="col-12 col-md-5 m-1">
           <RenderDish selectedDish={props.dish} />
         </div>
         <div className="col-12 col-md-5 m-1">
-          <RenderComments selectedComments={props.dish.comments} />
+          <RenderComments selectedComments={props.comments} />
         </div>
       </div>
+      </React.Fragment>
     );
   } else {
     return (
