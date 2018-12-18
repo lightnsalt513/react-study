9f853aa7a04fdfc94ecbbd090d09bf6085366c43
Jay Do <jaydo1204@gmail.com>
2018-12-18 23:58:16 +0900

    React Router Task 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -37,30 +37,30 @@ function RenderComments({selectedComments}) {
 const DishDetail = (props) => {
   if (props.dish != null) {
     return (
-      <React.Fragment>
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to='/menu'>Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>
-            {props.dish.name}
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to='/menu'>Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>
+              {props.dish.name}
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish selectedDish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments selectedComments={props.comments} />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish selectedDish={props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <RenderComments selectedComments={props.comments} />
-        </div>
-      </div>
-      </React.Fragment>
     );
   } else {
     return (
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import { DISHES } from '../shared/dishes';
@@ -49,8 +50,9 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-          <Route path="/menu/:dishId" component={DishWithId} />} />
-          <Route exact path="/contactus" component={Contact} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
